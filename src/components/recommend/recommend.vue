3c8078ab96e22a2c3cb88515f71ae10fbd42329d
electricUnion <532617457@qq.com>
2018-02-02 18:06:08 +0800

    2.222222

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-      <div class="siide-wrapper">
+      <div class="slide-wrapper" v-if="recommends.length">
+        <slider>
+          <div v-for="item in recommends" :key="item.picUrl">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
       </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
@@ -13,6 +20,31 @@
 </template>
 
 <script type="text/ecmascript-6">
+import { getRecommend } from 'api/recommend'
+import Slider from 'base/slider/slider'
+import { ERR_OK } from 'api/config'
+export default {
+  data () {
+    return {
+      recommends: []
+    }
+  },
+  created () {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend () {
+      getRecommend().then(res => {
+        if (res.code === ERR_OK) {
+          this.recommends = res.data.slider
+        }
+      })
+    }
+  },
+  components: {
+    Slider
+  }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
