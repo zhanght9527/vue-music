f9fdec049558e994274c8c738eadae8bf66ebfe3
electricUnion <532617457@qq.com>
2018-02-02 09:33:09 +0800

    2.2

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,7 +1,7 @@
 import jsonp from 'common/js/jsonp'
 import {commonParams, options} from './config'
 
-export function getRecommend() {
+export function getRecommend () {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
 
   const data = Object.assign({}, commonParams, {
