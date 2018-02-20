701542e1c8a838139f9e102db6a5c70225892020
张华挺 <532617457@qq.com>
2018-02-20 15:49:13 +0800

    2.20

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -17,7 +17,7 @@
     <scroll :data="songs" class="list" ref="list"
             :probe-type="probeType" :listen-scroll="listenScroll" @scroll="scroll">
       <div class="song-list-wrapper">
-        <song-list :songs="songs"></song-list>
+        <song-list @select="selectItem" :songs="songs"></song-list>
       </div>
       <div class="loading-content" v-show="!songs.length">
         <loading></loading>
@@ -31,6 +31,7 @@ import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
 import { prefixStyle } from 'common/js/dom'
 import Loading from 'base/loading/loading'
+import { mapActions } from 'vuex'
 
 const RESERVED_HEIGHT = 40
 const transform = prefixStyle('transform')
@@ -75,7 +76,16 @@ export default {
     },
     back () {
       this.$router.back()
-    }
+    },
+    selectItem (item, index) {
+      this.selectPlay({
+        list: this.songs,
+        index
+      })
+    },
+    ...mapActions([
+      'selectPlay'
+    ])
   },
   watch: {
     scrollY (newY) {
