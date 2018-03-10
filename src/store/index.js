9c538ca46309b8aef8cc9e944088d42c65e07674
electricUnion <532617457@qq.com>
2018-03-10 10:49:04 +0800

    final

--- src/store/index.js
+++ src/store/index.js
@@ -5,17 +5,12 @@ import * as actions from './actions'
 import mutations from './mutations'
 import * as getters from './getters'
 import state from './state'
-import createLogger from 'vuex/dist/logger'
 
 Vue.use(Vuex)
-
-const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
   actions,
   getters,
   state,
-  mutations,
-  strict: debug,
-  plugins: debug ? [createLogger()] : []
+  mutations
 })
